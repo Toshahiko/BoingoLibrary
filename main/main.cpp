524370cd6b0ddef6caf0c1c595ac2f739ab90c97
tanimoto <04girl.u.want@gmail.com>
2021-08-14 08:46:45 +0900

    mainを一時保存

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,14 @@
 #include <include/pch.h>
 #include "src/HelloFunc.h"
 #include <src/SettingManager.h>
-#include <include/Debugger.hpp>
+#include <include/Debugger.h>
+#include <include/Vector3D.h>
+#include <matplot/matplot.h>
+
+namespace {
+  void FourierTransform() {
+  }
+}
 
 int main() {
   Boingo::HelloFunc() ;
@@ -10,7 +17,17 @@ int main() {
 
   const auto anony = Boingo::Debug::decorate_debug( [](){ return 0 ;}, "anonymous" ) ;
 
+  Boingo::Math::Vector3D vec ;
+  const auto vec2 = -vec ;
+
   anony();
   std::vector<int> a ;
+
+  using namespace matplot;
+    auto [x, y, z] = peaks();
+    surf(x, y, z);
+    // save("img/peaks.gif");
+    // save("img/peaks.eps");
+    show();
   return 0 ;
 }
