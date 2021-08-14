806182204bdefb5fe5ac5c5c402c1b7f8e426cf3
tanimoto <04girl.u.want@gmail.com>
2021-08-14 20:27:33 +0900

    バックトラック法を導入

--- main/main.cpp
+++ main/main.cpp
@@ -23,12 +23,12 @@ const auto DivergeFuncY( double x, double y ) {
 
 class Newton2D {
   public:
-  Newton2D( const auto& diverge_x, const auto& diverge_y, const auto& initial_value, const double delta )
-  : m_diverge_x( diverge_x ), m_diverge_y( diverge_y ), m_initial_value( initial_value ), m_delta( delta )
+  Newton2D( const auto& diverge_x, const auto& diverge_y, const auto& initial_value, const double step_width )
+  : m_diverge_x( diverge_x ), m_diverge_y( diverge_y ), m_initial_value( initial_value ), m_step_width( step_width )
   {}
 
   std::pair<matplot::vector_1d, matplot::vector_1d>
-  Execute() const {
+  Execute() {
     auto tmp_xy = m_initial_value ;
 
     matplot::vector_1d x_plot ;
@@ -45,8 +45,9 @@ class Newton2D {
       const auto diverge_x = m_diverge_x( tmp_xy.first, tmp_xy.second ) ;
       const auto diverge_y = m_diverge_y( tmp_xy.first, tmp_xy.second ) ;
       if ( norm( diverge_x, diverge_y ) < tolerance ) break ;
-      tmp_xy.first = tmp_xy.first - m_delta*diverge_x ;
-      tmp_xy.second = tmp_xy.second - m_delta*diverge_y ;
+      const auto step_width = StepWidth( tmp_xy.first, tmp_xy.second, diverge_x, diverge_y ) ;
+      tmp_xy.first = tmp_xy.first - step_width*diverge_x ;
+      tmp_xy.second = tmp_xy.second - step_width*diverge_y ;
 
       x_plot.push_back( tmp_xy.first ) ;
       y_plot.push_back( tmp_xy.second ) ;
@@ -60,10 +61,24 @@ class Newton2D {
   }
 
   private:
+  double StepWidth( const double x, const double y, const double d_x, const double d_y ) {
+    constexpr double rho = 0.8, c1 = 0.8 ;
+
+    for ( int i = 0 ; i < 100 ; ++i ) {
+      const auto lhs = func( x - m_step_width*d_x, y - m_step_width*d_y ) ;
+      const auto rhs = func( x, y ) - c1*m_step_width*( d_x*d_x + d_y*d_y ) ;
+      if ( lhs < rhs ) break ;
+      m_step_width *= rho ;
+    }
+
+    return m_step_width ;
+  }
+
+  private:
     const std::function<double(double,double)> m_diverge_x ;
     const std::function<double(double,double)> m_diverge_y ;
     const std::pair<double, double>            m_initial_value ;
-    const double                               m_delta ;
+          double                               m_step_width ;
 } ;
 
 }
